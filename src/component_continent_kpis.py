ba5bccafd92c50f5ed4b56057a57df8078cd6b6d
Luke Collins <luke.a.collins@outlook.com>
2022-03-04 17:59:24 -0800

    feat: put KPI components in cards. Add card headers

--- src/component_continent_kpis.py
+++ src/component_continent_kpis.py
@@ -2,133 +2,135 @@ from dash import html
 import dash_bootstrap_components as dbc
 
 continent_kpi_cards = dbc.Card(
-    dbc.CardBody(
-        [
-            html.H4("Continent Key Stats", className="card-title"),
-            dbc.Row(
-                [
-                    dbc.Col(
-                        dbc.Card(
-                            [
-                                dbc.CardBody(
-                                    [
-                                        html.H5(
-                                            children="GDP",
-                                            className="card-title",
-                                            style={
-                                                "font-size": "32px",
-                                                "font-weight": "lighter",
-                                                "text-align": "center",
-                                            },
-                                        ),
-                                        html.H5(
-                                            children="mean",
-                                            className="card-title",
-                                            style={
-                                                "font-size": "16px",
-                                                "font-weight": "lighter",
-                                                "text-align": "center",
-                                            },
-                                        ),
-                                        html.P(
-                                            id="continent-mean-gdp",
-                                            children="-",
-                                            className="card-text",
-                                            style={
-                                                "font-size": "24px",
-                                                "font-weight": "bold",
-                                                "text-align": "center",
-                                            },
-                                        ),
-                                    ]
-                                ),
-                            ],
-                            color="primary",
-                            inverse=True,
-                        )
-                    ),
-                    dbc.Col(
-                        dbc.Card(
-                            [
-                                dbc.CardBody(
-                                    [
-                                        html.H5(
-                                            children="Population",
-                                            className="card-title",
-                                            style={
-                                                "font-size": "32px",
-                                                "font-weight": "lighter",
-                                                "text-align": "center",
-                                            },
-                                        ),
-                                        html.H5(
-                                            children="mean",
-                                            className="card-title",
-                                            style={
-                                                "font-size": "16px",
-                                                "font-weight": "lighter",
-                                                "text-align": "center",
-                                            },
-                                        ),
-                                        html.P(
-                                            id="continent-mean-pop",
-                                            children="-",
-                                            className="card-text",
-                                            style={
-                                                "font-size": "24px",
-                                                "font-weight": "bold",
-                                                "text-align": "center",
-                                            },
-                                        ),
-                                    ]
-                                ),
-                            ],
-                            color="info",
-                            inverse=True,
-                        )
-                    ),
-                    dbc.Col(
-                        dbc.Card(
-                            [
-                                dbc.CardBody(
-                                    [
-                                        html.H5(
-                                            children="Life Expectancy",
-                                            className="card-title",
-                                            style={
-                                                "font-size": "32px",
-                                                "font-weight": "lighter",
-                                                "text-align": "center",
-                                            },
-                                        ),
-                                        html.H5(
-                                            children="mean",
-                                            className="card-title",
-                                            style={
-                                                "font-size": "16px",
-                                                "font-weight": "lighter",
-                                                "text-align": "center",
-                                            },
-                                        ),
-                                        html.P(
-                                            id="continent-mean-lifeexp",
-                                            children="-",
-                                            className="card-text",
-                                            style={
-                                                "font-size": "24px",
-                                                "font-weight": "bold",
-                                                "text-align": "center",
-                                            },
-                                        ),
-                                    ]
-                                ),
-                            ],
-                            color="secondary",
-                            inverse=True,
-                        )
-                    ),
-                ]
-            ),
-        ]
-    ),
+    [
+        dbc.CardHeader([html.H4("Continent Key Stats", className="card-title")]),
+        dbc.CardBody(
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            dbc.Card(
+                                [
+                                    dbc.CardBody(
+                                        [
+                                            html.H5(
+                                                children="GDP",
+                                                className="card-title",
+                                                style={
+                                                    "font-size": "32px",
+                                                    "font-weight": "lighter",
+                                                    "text-align": "center",
+                                                },
+                                            ),
+                                            html.H5(
+                                                children="mean",
+                                                className="card-title",
+                                                style={
+                                                    "font-size": "16px",
+                                                    "font-weight": "lighter",
+                                                    "text-align": "center",
+                                                },
+                                            ),
+                                            html.P(
+                                                id="continent-mean-gdp",
+                                                children="-",
+                                                className="card-text",
+                                                style={
+                                                    "font-size": "24px",
+                                                    "font-weight": "bold",
+                                                    "text-align": "center",
+                                                },
+                                            ),
+                                        ]
+                                    ),
+                                ],
+                                color="primary",
+                                inverse=True,
+                            )
+                        ),
+                        dbc.Col(
+                            dbc.Card(
+                                [
+                                    dbc.CardBody(
+                                        [
+                                            html.H5(
+                                                children="Population",
+                                                className="card-title",
+                                                style={
+                                                    "font-size": "32px",
+                                                    "font-weight": "lighter",
+                                                    "text-align": "center",
+                                                },
+                                            ),
+                                            html.H5(
+                                                children="mean",
+                                                className="card-title",
+                                                style={
+                                                    "font-size": "16px",
+                                                    "font-weight": "lighter",
+                                                    "text-align": "center",
+                                                },
+                                            ),
+                                            html.P(
+                                                id="continent-mean-pop",
+                                                children="-",
+                                                className="card-text",
+                                                style={
+                                                    "font-size": "24px",
+                                                    "font-weight": "bold",
+                                                    "text-align": "center",
+                                                },
+                                            ),
+                                        ]
+                                    ),
+                                ],
+                                color="info",
+                                inverse=True,
+                            )
+                        ),
+                        dbc.Col(
+                            dbc.Card(
+                                [
+                                    dbc.CardBody(
+                                        [
+                                            html.H5(
+                                                children="Life Expectancy",
+                                                className="card-title",
+                                                style={
+                                                    "font-size": "32px",
+                                                    "font-weight": "lighter",
+                                                    "text-align": "center",
+                                                },
+                                            ),
+                                            html.H5(
+                                                children="mean",
+                                                className="card-title",
+                                                style={
+                                                    "font-size": "16px",
+                                                    "font-weight": "lighter",
+                                                    "text-align": "center",
+                                                },
+                                            ),
+                                            html.P(
+                                                id="continent-mean-lifeexp",
+                                                children="-",
+                                                className="card-text",
+                                                style={
+                                                    "font-size": "24px",
+                                                    "font-weight": "bold",
+                                                    "text-align": "center",
+                                                },
+                                            ),
+                                        ]
+                                    ),
+                                ],
+                                color="secondary",
+                                inverse=True,
+                            )
+                        ),
+                    ]
+                ),
+            ]
+        ),
+    ]
 )
--- src/component_countries_kpis.py
+++ src/component_countries_kpis.py
@@ -17,123 +17,134 @@ countries_kpi_type_div = [
 ]
 
 countries_kpi_cards_div = [
-    dbc.Row(countries_kpi_type_div),
-    dbc.Row(
+    dbc.Card(
         [
-            dbc.Col(
-                dbc.Card(
-                    [
-                        dbc.CardBody(
-                            [
-                                html.H5(
-                                    "GDP",
-                                    className="card-title",
-                                    style={
-                                        "font-size": "18px",
-                                        "font-weight": "bold",
-                                    },
-                                ),
-                                html.P(
-                                    id="gdp_country",
-                                    children="-",
-                                    className="card-text",
-                                    style={
-                                        "font-size": "24px",
-                                        "font-weight": "bold",
-                                    },
-                                ),
-                                html.P(
-                                    id="gdp_value",
-                                    children="-",
-                                    className="card-text",
-                                    style={
-                                        "font-size": "14px",
-                                    },
-                                ),
-                            ]
-                        ),
-                    ],
-                    color="success",
-                    inverse=True,
-                ),
-                width=4,
+            dbc.CardHeader(
+                [html.H4("Country High-Low Key Stats", className="card-title")]
             ),
-            dbc.Col(
-                dbc.Card(
-                    [
-                        dbc.CardBody(
-                            [
-                                html.H5(
-                                    "Population",
-                                    className="card-title",
-                                    style={
-                                        "font-size": "18px",
-                                        "font-weight": "bold",
-                                    },
+            dbc.CardBody(
+                [
+                    dbc.Row(countries_kpi_type_div),
+                    dbc.Row(
+                        [
+                            dbc.Col(
+                                dbc.Card(
+                                    [
+                                        dbc.CardBody(
+                                            [
+                                                html.H5(
+                                                    "GDP",
+                                                    className="card-title",
+                                                    style={
+                                                        "font-size": "18px",
+                                                        "font-weight": "bold",
+                                                    },
+                                                ),
+                                                html.P(
+                                                    id="gdp_country",
+                                                    children="-",
+                                                    className="card-text",
+                                                    style={
+                                                        "font-size": "24px",
+                                                        "font-weight": "bold",
+                                                    },
+                                                ),
+                                                html.P(
+                                                    id="gdp_value",
+                                                    children="-",
+                                                    className="card-text",
+                                                    style={
+                                                        "font-size": "14px",
+                                                    },
+                                                ),
+                                            ]
+                                        ),
+                                    ],
+                                    color="success",
+                                    inverse=True,
                                 ),
-                                html.P(
-                                    id="pop_country",
-                                    children="-",
-                                    className="card-text",
-                                    style={
-                                        "font-size": "24px",
-                                        "font-weight": "bold",
-                                    },
+                                width=4,
+                            ),
+                            dbc.Col(
+                                dbc.Card(
+                                    [
+                                        dbc.CardBody(
+                                            [
+                                                html.H5(
+                                                    "Population",
+                                                    className="card-title",
+                                                    style={
+                                                        "font-size": "18px",
+                                                        "font-weight": "bold",
+                                                    },
+                                                ),
+                                                html.P(
+                                                    id="pop_country",
+                                                    children="-",
+                                                    className="card-text",
+                                                    style={
+                                                        "font-size": "24px",
+                                                        "font-weight": "bold",
+                                                    },
+                                                ),
+                                                html.P(
+                                                    id="pop_value",
+                                                    children="-",
+                                                    className="card-text",
+                                                    style={
+                                                        "font-size": "14px",
+                                                    },
+                                                ),
+                                            ]
+                                        ),
+                                    ],
+                                    color="warning",
+                                    inverse=True,
                                 ),
-                                html.P(
-                                    id="pop_value",
-                                    children="-",
-                                    className="card-text",
-                                    style={
-                                        "font-size": "14px",
-                                    },
+                                width=4,
+                            ),
+                            dbc.Col(
+                                dbc.Card(
+                                    [
+                                        dbc.CardBody(
+                                            [
+                                                html.H5(
+                                                    children="Life Expectancy",
+                                                    className="card-title",
+                                                    style={
+                                                        "font-size": "18px",
+                                                        "font-weight": "bold",
+                                                    },
+                                                ),
+                                                html.P(
+                                                    id="lifeexp_country",
+                                                    children="-",
+                                                    className="card-text",
+                                                    style={
+                                                        "font-size": "24px",
+                                                        "font-weight": "bold",
+                                                    },
+                                                ),
+                                                html.P(
+                                                    id="lifeexp_value",
+                                                    children="-",
+                                                    className="card-text",
+                                                    style={
+                                                        "font-size": "14px",
+                                                    },
+                                                ),
+                                            ]
+                                        ),
+                                    ],
+                                    color="danger",
+                                    inverse=True,
                                 ),
-                            ]
-                        ),
-                    ],
-                    color="warning",
-                    inverse=True,
-                ),
-                width=4,
-            ),
-            dbc.Col(
-                dbc.Card(
-                    [
-                        dbc.CardBody(
-                            [
-                                html.H5(
-                                    children="Life Expectancy",
-                                    className="card-title",
-                                    style={
-                                        "font-size": "18px",
-                                        "font-weight": "bold",
-                                    },
-                                ),
-                                html.P(
-                                    id="lifeexp_country",
-                                    children="-",
-                                    className="card-text",
-                                    style={
-                                        "font-size": "24px",
-                                        "font-weight": "bold",
-                                    },
-                                ),
-                                html.P(
-                                    id="lifeexp_value",
-                                    children="-",
-                                    className="card-text",
-                                    style={
-                                        "font-size": "14px",
-                                    },
-                                ),
-                            ]
-                        ),
-                    ],
-                    color="danger",
-                    inverse=True,
-                ),
-                width=4,
+                                width=4,
+                            ),
+                        ]
+                    ),
+                ]
             ),
         ]
-    ),
+    )
 ]
